335af9691357a4a75db820afb5036c5147c817f8
pallavichittimalla <pallavichittimalla99@gmail.com>
2025-02-07 14:51:19 +0530

    functions

---
+++ func.js
@@ -0,0 +1,50 @@
+//function greet(name){
+ //   return `Hello,${name}!`;
+
+//}
+//console.log(greet("pallavi"));
+//func exp
+/*const greet=function(name){
+    return `hello,${name}!`;
+
+}
+console.log(greet("pallavi")); */
+//arrow func
+/*
+const greet=(name)=>`Hello, ${name}`;
+console.log(greet("pallavi"));
+*/
+//immed invked func exp
+/*
+(function(){
+    console.log("this is pallavi");
+})();  */
+
+//call back
+/*
+const greetfn=(name)=>{
+console.log(`Hello,${name}!`);
+}
+function processName(callback){
+    const name="pallavi";
+callback(name);
+}
+processName(greetfn);
+*/
+
+//objects
+/*
+person={
+    name:"pallavi",
+    age:20,
+greet:function(){
+    return `Hello,my name is ${this.name}`;
+},
+};
+console.log(person.name);
+console.log(person.greet());*/
+
+
+
+
+
